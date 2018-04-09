052e689f1e9fbb2a6ba38d8c3b7a837dd592a7be
Indranil Ghosh <indranilg49@gmail.com>
2018-04-10 01:36:06 +0530

    Add files via upload

---
+++ ran_perm.py
@@ -0,0 +1,17 @@
+import random
+
+def nran(k, l):
+        while True:
+                m=int(random.uniform(k, l+1))
+                if m<=l: return m
+def ran_perm(K):
+        P=range(1, K+1)
+        print(P)
+        for k in range(1, K):
+##                l=random.choice(range(k, K))
+                l=nran(k, K)
+                temp=P[l-1]
+                P[l-1]=P[k-1]
+                P[k-1]=temp
+                print(P)
+        return P
