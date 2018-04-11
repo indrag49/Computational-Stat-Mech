11f92229717539b58cae16c508cfa48d36296d6d
Indranil Ghosh <indranilg49@gmail.com>
2018-04-12 00:28:32 +0530

    Update markov_discrete_pebble.py

--- markov_discrete_pebble.py
+++ markov_discrete_pebble.py
@@ -1,11 +1,15 @@
-
 import numpy as np
 import random
-
 A=np.array(([2, 4, 0, 0], [3, 5, 1, 0], [0, 6, 2, 0],
               [5, 7, 0, 1], [6, 8, 4, 2], [0, 9, 5, 3],
               [8, 0, 0, 4], [9, 0, 7, 5], [0, 0, 8, 6]))
-print (A)
+print A
+
+def nran(k, l):
+        while True:
+                m=int(random.uniform(k, l+1))
+                if m<=l: return m
+                
 def Nbr(n, k): return A.T[n-1, k-1]
 
 def markov_discrete_pebble(k):
