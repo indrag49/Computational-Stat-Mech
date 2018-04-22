ee241613aa037738e6d4c2ebd5955c89f9967f07
Indranil Ghosh <indranilg49@gmail.com>
2018-04-23 00:52:05 +0530

    Update direct_gamma_zeta.py

--- direct_gamma_zeta.py
+++ direct_gamma_zeta.py
@@ -4,4 +4,9 @@ def direct_gamma_zeta(gamma, zeta, N):
         for i in range(N):
                 x=random.uniform(0, 1)**(1/(zeta+1.))
                 S+=x**(gamma-zeta)
-        return S/N
+                s+=x**(2*(gamma-zeta))
+        mean=S*1./N
+        y=s*1./N
+        var=m.sqrt(y-mean**2)
+        error=var/m.sqrt(N)
+        return [mean, error]
