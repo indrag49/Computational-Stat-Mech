970b684e02189fb3ca61c31e4ea14219bdaa6ac6
Indranil Ghosh <indranilg49@gmail.com>
2018-05-02 00:09:49 +0530

    Update wall_time.py

--- Chapter 2/wall_time.py
+++ Chapter 2/wall_time.py
@@ -1,2 +1,2 @@
 from sympy import oo
-def wall_time(pos, vel, radius): return (1.0-radius-pos)/vel if vel>0.0 else (pso-radius)/abs(vel) if vel<0.0 else float(oo)
+def wall_time(pos, vel, radius): return (1.0-radius-pos)/vel if vel>0.0 else (pos-radius)/abs(vel) if vel<0.0 else float(oo)
