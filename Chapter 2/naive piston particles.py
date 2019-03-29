5f15762a319055c562aa793ce8e35ffa823c14e3
Indranil Ghosh <indranilg49@gmail.com>
2019-03-30 00:08:37 +0530

    Add files via upload

--- Chapter 2/naive piston particles.py
+++ Chapter 2/naive piston particles.py
@@ -13,3 +13,14 @@ def naive_piston_particles(N, L, T, P, delta):
     if random.uniform(0,1)<Upsilon and L+Delta_L>xmax:
         L+=Delta_L
     return [L, x]
+
+def naive_piston_particles_patch(N, L, T, P, delta):
+    Kb=1.3807*10**(-23)
+    Beta=1/(Kb*T)
+    x=[0]*N
+    for i in range(N):
+        x[i]=random.uniform(0,L)
+    xmax=max(x)
+    Delta_L=-math.log(random.uniform(0, 1))/(Beta*P)
+    L=xmax+Delta_L
+    return [L, x]
